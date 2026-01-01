013966aacc96d78fc011647dc0dfa57078ec5ad9
Roni Egbu <ronniiii@yahoo.com>
2026-01-01 17:19:41 +0100

    feat: replace UploadButton with UploadDropzone for improved media upload experience

--- src/app/groups/[groupID]/UploadZone.tsx
+++ src/app/groups/[groupID]/UploadZone.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { UploadButton } from "@/utils/uploadthing";
+import { UploadDropzone } from "@/utils/uploadthing";
 import { useRouter } from "next/navigation";
 
 import "@uploadthing/react/styles.css";
@@ -11,17 +11,25 @@ export default function UploadZone({ groupId }: { groupId: string }) {
   return (
     <div className="rounded-xl bg-white/10 p-8 shadow-xl">
       <h2 className="mb-4 text-xl font-semibold">Upload New Media</h2>
-      <UploadButton
-        endpoint="mediaUploader"
-        input={{ groupId }}
-        onClientUploadComplete={() => {
-          alert("Saved to database!");
-          router.refresh();
-        }}
-        onUploadError={(error: Error) => {
-          alert(`ERROR! ${error.message}`);
-        }}
-      />
+
+      <div className="w-full max-w-3xl rounded-3xl border border-dashed border-white/20 bg-white/5 p-2 transition-colors hover:border-[hsl(280,100%,70%)]/50">
+        <UploadDropzone
+          endpoint="mediaUploader"
+          input={{ groupId }}
+          appearance={{
+            button:
+              "!bg-[hsl(280,100%,60%)] text-white px-4 py-2 rounded-xl after:bg-white/20",
+            container: "border-none bg-transparent py-10",
+            label: "!text-gray-50 hover:!text-[hsl(280,100%,70%)]",
+          }}
+          onClientUploadComplete={() => {
+            router.refresh();
+          }}
+          onUploadError={(error: Error) => {
+            alert(`ERROR! ${error.message}`);
+          }}
+        />
+      </div>
     </div>
   );
 }
